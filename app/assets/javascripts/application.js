34bcfdca74c0fff77c3e06f321a6cab08df3a0df
Ryan Yep <ryan.yep@gmail.com>
2015-11-02 18:07:23 -0600

    CSS formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,3 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 });
-
-$(document).ready(function(){
-   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-   $('.modal-trigger').leanModal();
- });
