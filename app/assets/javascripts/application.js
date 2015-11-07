9800d1a2a2b0a9dacf96236abf7635076aaf9d4c
Ryan Yep <ryan.yep@gmail.com>
2015-11-07 12:53:04 -0600

    Add padding to logo nav bar and ul links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,8 @@
 $(document).ready(function(){
   $('.modal-trigger').leanModal();
 });
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 100 // Creates a dropdown of 100 years to control year
+});
