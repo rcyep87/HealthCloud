38f9de988439c1784f545590384136657c4c8112
Ryan Yep <ryan.yep@gmail.com>
2016-01-25 16:38:26 -0600

    Verified as i try to implement oauth with Fitbit, it is dependent upon omniauth-fibit gem

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def fitbit
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    sign_in_and_redirect user_omniauth_authorize_path(@user)
   end
 
 end
